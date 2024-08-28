95e2c50b027c410b8c6d2a197abb860e10c2f5b8
dxnielemotta <danielemottarocha@gmail.com>
2024-08-27 22:44:21 -0300

    feat: configura banco de dados e adiciona .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
